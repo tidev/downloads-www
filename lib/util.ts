47325fe281890f58af826b35b758e775b5418986
Hans Knöchel <h.knoechel@lambus.com>
2024-02-12 19:15:33 +0100

    fix: sort by date

--- lib/util.ts
+++ lib/util.ts
@@ -4,23 +4,5 @@ const re = /^(\d+)\.(\d+)\.(\d+)\.(\w+)$/;
 export const types = [ 'beta', 'rc', 'ga' ];
 
 export function sortLatest(a: BuildInfo, b: BuildInfo) {
-	const [ _, amajor, aminor, apatch, atag ] = a.name.toLowerCase().match(re);
-	const [ __, bmajor, bminor, bpatch, btag ] = b.name.toLowerCase().match(re);
-	
-	let n = parseInt(bmajor) - parseInt(amajor);
-	if (n !== 0) {
-		return n;
-	}
-
-	n = parseInt(bminor) - parseInt(aminor);
-	if (n !== 0) {
-		return n;
-	}
-
-	n = parseInt(bpatch) - parseInt(apatch);
-	if (n !== 0) {
-		return n;
-	}
-
-	return types.indexOf(btag) - types.indexOf(atag);
+	return new Date(b.date).getTime() - new Date(a.date).getTime()
 }
